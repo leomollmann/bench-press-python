9d772a480dbeee7257c448f93d8fae9a3ac2ffb6
leomollmann <leonardo.scholler@poatek.com>
2023-05-10 11:55:39 -0300

    created basic API

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from functools import wraps
 from flask import Flask, abort, json, request
 from flask_cors import CORS
+from gpt import chat_gpt_repository
 
 app = Flask(__name__)
 
@@ -17,10 +18,41 @@ def handle_internal_error(f):
 
   return decorator
 
-@app.route('/api/healthcheck', methods=['GET'])
-def healthcheck():  
+@app.route('/api/create_user', methods=['POST'])
+def createUser():
+  user = request.get_json()
+
+  chat_gpt_repository.create_user({
+    'name': user['name'],
+    'checkin': user['checkin'],
+    'checkout': user['checkout'],
+    'bill': user['bill']
+  })
+
   return app.response_class(
-    response=json.dumps(datetime.now()),
+    status=200,
+    mimetype='application/json'
+  )
+
+@app.route('/api/get_history', methods=['GET'])
+def getHistory(): 
+  user = request.args.get("user")
+  history = chat_gpt_repository.get_hisotry(user)
+  
+  return app.response_class(
+    response=json.dumps(history),
+    status=200,
+    mimetype='application/json'
+  )
+
+@app.route('/api/chat', methods=['POST'])
+def addMessage():
+  user = request.args.get("user")
+  data = request.get_json()
+  response = chat_gpt_repository.chat(user, data['chat'])
+
+  return app.response_class(
+    response=json.dumps(response),
     status=200,
     mimetype='application/json'
   )
---
+++ gpt.py
@@ -0,0 +1,54 @@
+import os
+import json
+import openai
+
+class ChatApp:
+  def __init__(self):
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    with open('./data/initial_prompt.json') as json_file:
+      self.initial_prompt = json.load(json_file)
+
+  def create_user(self, data):
+    messages = [self.initial_prompt]
+    messages.append({
+      "role": "system", 
+      "content": f"""This guest's data is described as: 
+        Name: {data['name']}
+        Check-in date: {data['checkin']}
+        Check-out date: {data['checkout']}
+        Currency: USD
+        Total price of stay: ${data['bill']}
+        """
+    })
+    
+    with open(f"./users/{data['name']}.json", 'w') as outfile:
+      json.dump(messages, outfile)
+
+  def get_hisotry(self, user):
+    messages = []
+    with open(f"./users/{user}.json") as json_file:
+      messages = json.load(json_file)
+
+    return [message for message in messages if message['role'] != 'system']
+
+  def chat(self, user, message):
+    messages = []
+    with open(f"./users/{user}.json") as json_file:
+      messages = json.load(json_file)
+
+    messages.append({"role": "user", "content": message})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    messages.append({
+      "role": "assistant", 
+      "content": response["choices"][0]["message"].content
+    })
+
+    with open(f'./users/{user}.json', 'w') as outfile:
+      json.dump(messages, outfile)
+
+    return response["choices"][0]["message"].content
+  
+chat_gpt_repository = ChatApp()
